f7984305d3c664b7d61d5a826f801b36c48b6d23
Fiddykhoum <fiddy@orange.fr>
2022-09-03 20:10:06 +0200

    First commit

---
+++ src/Tabs.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import './Tabs.css';
+import ESC from './esc.jpg';
+
+const Tabs = () => {
+  const [tabs, setTabs] = useState([
+    { 
+      id: Math.random(),
+      label: 'Description du produit',
+      content: (
+        <div>
+            <h2>Sire - Marcus Miller </h2>
+            <strong>S7 5 strings - BRS 2nd Gen  Corps en Aulne</strong>
+              <ul> 
+                <li>forme du corps: New modern bass</li>
+                <li>manche en érable</li>
+                <li>forme en C</li>
+                <li>diapason: 34"</li>
+                <li>touche en Ebène (Radius : 12")</li>
+                <li>Frets: medium</li>
+                <li>24 frets</li>
+                <li>sillet:38mm,Incrustations nacrées</li>
+                <li>capteur Marcus Pure-H Revolution Set</li>
+                <li>Pré-ampli: Marcus Heritage- 3 (avec ajustement medium)</li>
+                <li>Contrôles : Volume / Tone (Dual Pot) | Pickup Blender | Treble | Middle / Middle Frequency (Dual Pot) | Bass</li>
+                <li>Mini Toggle (Active / Passive ),Chevalet : Marcus Miller Heavy Mass Custom</li>
+                <li>Mécanique: Sire Diecasting Gear (Black)</li>
+                <li>4.20kg</li>
+                <li>Finition Brown Sunburst</li>
+              </ul>
+        </div>
+      )
+    },
+    { 
+      id: Math.random(),
+      label: 'Images',
+      content: (
+        <div>
+          <img 
+            className= "Basspic"
+            src={ESC} 
+            alt="Basse Sire"
+          />
+        </div>
+      )
+    },
+    { 
+      id: Math.random(),
+      label: 'Commentaires',
+      content: (
+        <div>
+          <ul>
+            <li>Superbe basse 5 cordes!</li>
+            <li>Son unique, belle finition.</li>
+            <li>Une marque qui a fait ses preuves.</li>
+          </ul>
+        </div>
+      )
+     }
+  ]);
+
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    
+    <div className="Wrapper">
+      <h1>Sire bass</h1>
+      <ul className="Tabs">
+        {tabs.map((tab) => {
+          return (
+            <li
+              key={tab.id}
+              onClick={() => handleTabClick(tab)}
+              className="Tab"
+            >
+              {tab.label}
+            </li>
+          );
+        })}
+      </ul>
+      <div className="ActiveTabContent">
+        {activeTab.content}
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
